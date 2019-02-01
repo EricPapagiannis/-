641662de562de8c24f31d3b9103194bb7c52473f
EricPapagiannis <eric.papagiannis@mail.utoronto.ca>
2019-02-01 00:34:15 -0500

    b

--- content.js
+++ content.js
@@ -1,21 +1,20 @@
 function replaceText (node) {
-    if (node.nodeName === "noscript" || node.nodeName === "script" || node.nodeName === "style")
+    if (node.nodeName === "NOSCRIPT" || node.nodeName === "SCRIPT" || node.nodeName === "STYLE")
     {
         return;
     }
     if (node.nodeType === Node.TEXT_NODE) {
         if (node.parentNode &&
-            node.parentNode.nodeName === 'TEXTAREA') {
-        return;
+            node.parentNode.nodeName === "TEXTAREA") {
+            return;
         }
-        
         node.textContent = node.textContent.replace(/b/gi, '🅱️');;
     }
     else {
-      for (let i = 0; i < node.childNodes.length; i++) {
+        for (let i = 0; i < node.childNodes.length; i++) {
         replaceText(node.childNodes[i]);
-      }    
+        }    
     }
-  }  
-  replaceText(document.body);
-  
+}  
+
+replaceText(document.body);
