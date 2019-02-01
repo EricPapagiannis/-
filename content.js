2cdcbd1bb09c4b57e44869e305bf5b5e9869a5bd
EricPapagiannis <eric.papagiannis@mail.utoronto.ca>
2019-02-01 01:41:06 -0500

    b

--- content.js
+++ content.js
@@ -1,19 +1,22 @@
 function replaceText (node) {
-    if (node.nodeName === "NOSCRIPT" || node.nodeName === "SCRIPT" || node.nodeName === "STYLE")
-    {
-        return;
-    }
-    if (node.nodeType === Node.TEXT_NODE) {
-        if (node.parentNode &&
-            node.parentNode.nodeName === "TEXTAREA") {
-            return;
-        }
-        node.textContent = node.textContent.replace(/b/gi, '🅱️');;
-    }
-    else {
-        for (let i = 0; i < node.childNodes.length; i++) {
-        replaceText(node.childNodes[i]);
-        }    
+    if (node) {
+        if (node.nodeName === "NOSCRIPT" || node.nodeName === "SCRIPT" || node.nodeName === "STYLE"
+        || node.nodeName === "CITE")
+       {
+           return;
+       }
+       if (node.nodeType === Node.TEXT_NODE) {
+           if (node.parentNode &&
+               node.parentNode.nodeName === "TEXTAREA") {
+               return;
+           }
+           node.textContent = node.textContent.replace(/b/gi, '🅱️');
+       }
+       else {
+           for (let i = 0; i < node.childNodes.length; i++) {
+           replaceText(node.childNodes[i]);
+           }    
+       }
     }
 }  
 
